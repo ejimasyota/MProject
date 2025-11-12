f96a9e094470e9bb44e6b9b0e2cb25f54c9f5136
ejimasyota <ejimasyota0902@icloud.com>
2025-11-12 01:23:50 +0000

    スタート画面パス指定修正

--- frontend/pages/Start.php
+++ frontend/pages/Start.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>スタート画面</title>
-  <link rel="stylesheet" href="asetts/css/style.css">
+  <link rel="stylesheet" href="../asetts/css/style.css">
 </head>
 <body>
   <div class="StartScreenContainer">
@@ -12,6 +12,6 @@
       <button class="ButtonInfo PinkButton" onClick="StartButtonEvent()">スタート</button>
     </div>
   </div>
-  <script src="script/page/Start.js"></script>
+  <script src="../script/page/Start.js"></script>
 </body>
 </html>
