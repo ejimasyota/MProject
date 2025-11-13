50e4edb9148ad0036ebf89ca3b55f7840007d85c
ejimasyota <ejimasyota0902@icloud.com>
2025-11-13 23:53:43 +0000

    SVGの調整

--- frontend/pages/Game.php
+++ frontend/pages/Game.php
@@ -30,7 +30,9 @@
 
 
       <button class="MenuButton" data-tooltip="ホームに戻る" aria-label="ホーム">
-        <svg viewBox="0 0 24 24"><path d="M12 3l9 8h-3v8h-12v-8H3l9-8z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="100" height="100">
+          <path d="M12 3l9 8h-3v8h-12v-8H3l9-8z"/>
+        </svg>
       </button>
     </div>
   </div>
