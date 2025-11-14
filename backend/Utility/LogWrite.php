8b524db0745f485a116b61d7e03ca50bf5cfbfac
ejimasyota <ejimasyota0902@icloud.com>
2025-11-14 03:00:03 +0000

    ログテスト

--- backend/Utility/LogWrite.php
+++ backend/Utility/LogWrite.php
@@ -5,23 +5,10 @@
  * @param string $message ログメッセージ
  */
 function writeLog($message) {
-    // ログディレクトリ
-    $logDir = __DIR__ . "/../Log";
+var_dump(__DIR__);
+var_dump(__DIR__ . "/../Log/Error");
+var_dump(is_writable(__DIR__ . "/../Log"));
+var_dump(is_writable(__DIR__ . "/../Log/Error"));
+exit;
 
-    // ログタイプごとのサブディレクトリに分ける
-    $logDir .= "/Error";
-
-    // ディレクトリがなければ作成
-    if (!is_dir($logDir)) {
-        mkdir($logDir, 0777, true);
-    }
-
-    // 日付ごとのログファイル
-    $logFile = $logDir . "/" . "まつのさ恋_" . date("Ymd") . ".log";
-
-    // タイムスタンプ付きメッセージ
-    $logMessage = "[" . date("Y-m-d H:i:s") . "] " . $message . PHP_EOL;
-
-    // ファイルへ追記
-    file_put_contents($logFile, $logMessage, FILE_APPEND | LOCK_EX);
 }
