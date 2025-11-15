28e20f3dc1bb012d9f289af3c21a68e571173b99
ejimasyota <ejimasyota0902@icloud.com>
2025-11-15 02:34:43 +0000

    コンソール追加

--- frontend/script/page/Game.js
+++ frontend/script/page/Game.js
@@ -157,6 +157,7 @@ function GameDisplayInfo(StoryId){
     *  2. ストーリーIDに紐づく要素を取得
     * --------------------------------------------*/
     const StoryItem = STORY_INFO.find(Item => {
+        console.log("Item",Item)
         // 1. storyidとStoryIdの値が完全一致するものを検索
         return Number(Item.storyid) === Number(StoryId);
     });
